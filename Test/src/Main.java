ed15530266bc316304d6fc028cc58b556459b6a3
1 <1@1-PC>
2020-03-17 14:02:56 +0900

    2020-03-17 ver 1.0

---
+++ Test/src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		
+		int c = a + b;
+		System.out.println(c);
+	}
+}
+
+/*
+	1. github생성 -> 연결(push, share)
+	2. 원격으로 연결 리무트 컨피그 어드벤스 마스터 (커밋 실행하기 위해서)
+	3. 
+ */
